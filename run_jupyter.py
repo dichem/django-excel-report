f4267acb4fe3214d794a8504d0cf3889cd40cf3b
borisalekseev <i.borisalekseev@gmail.com>
2023-04-11 16:10:06 +0300

    DJEX-8 файл с описанием создан и лежит в корне

---
+++ run_jupyter.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+import os
+import sys
+
+from django.core.management import execute_from_command_line
+
+
+def jupyter():
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tests.settings")
+    print(os.getcwd())
+    try:
+        import django_extensions
+    except ImportError:
+        raise ImportError("Необходимо установить django-extensions чтобы запустить юпитер (а также сам юпитер)")
+    argv = sys.argv + ['shell_plus', '--notebook']
+    execute_from_command_line(argv)
+
+
+if __name__ == "__main__":
+    jupyter()
